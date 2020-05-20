b72e2a2f88cf8663dd0144431f49158c9a84286c
Fight-Jason <18665308713@163.com>
2020-05-20 18:29:54 +0800

    reducer添加immutable对数据校验

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,20 @@
 import * as constants from './constants';
+import { fromJS } from 'immutable'
 
-const defaultState = {
+const defaultState = fromJS({
     focused: false
-};
+})
 
 export default (state = defaultState, action) => {
     if(action.type === constants.SEARCH_FOCUS ) {
-        return {
-            focused: true
-        }
+        // immutable对象的set方法
+        // 会结合之前immutable对象的值和设置的值，返回全新的对象
+        // 如果引用immutable再次获取对象要使用get方法
+        return state.set('focused', true)
     }
 
     if(action.type === constants.SEARCH_BLUR ) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false)
     }
     return state;
 }
