d75abe98e3764bc0c710b232f406b2d8a487bf88
Fight-Jason <18665308713@163.com>
2020-05-30 17:18:02 +0800

    添加首页文章加载更多功能

--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -1,17 +1,33 @@
 import axios from 'axios';
 import * as constants from './constants';
-
+import { fromJS } from 'immutable'
 export const changeHomeData = (result) => ({
     type:constants.CHANGE_HOME_DATA,
     articleList: result.articleList,
-    recommendList: result.recommendList    
+    recommendList: result.recommendList     
 })
+
+export const addHomeList = (list,nextPage) => ({
+    type: constants.ADD_ARTICLE_LIST,
+    list: fromJS(list),
+    nextPage
+})
+
 export const getHomeInfo = () => {
     return (dispatch) => {
         axios.get('/api/home.json').then((res) => {
             const result = res.data;
             const action = changeHomeData(result)
             dispatch(action)
+        })
+    }
+}
+
+export const getMoreList = (page) => {
+    return (dispatch) => {
+        axios.get('/api/homeList.json?page=' + page).then((res) => {
+            const result = res.data.articleList;
+            dispatch(addHomeList(result, page + 1));
         })
     }
 }
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -3,15 +3,21 @@ import * as constants from './constants';
 
 const defaultState = fromJS({
     articleList: [],
-    recommendList: []
+    recommendList: [],
+    articlePage: 1
 })
 
 export default (state = defaultState, action) => {
     switch(action.type) {
         case constants.CHANGE_HOME_DATA:
-        return state.merge({
-                articleList: fromJS(action.articleList),
-                recommendList:fromJS(action.recommendList)
+            return state.merge({
+                    articleList: fromJS(action.articleList),
+                    recommendList:fromJS(action.recommendList)
+                })
+        case constants.ADD_ARTICLE_LIST:
+            return state.merge({
+                'articleList': state.get('articleList').concat(action.list),
+                'articlePage': action.nextPage
             })
         default:
             return state
