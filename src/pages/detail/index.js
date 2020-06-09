c774f35d88e60dc93d52bc32c6e6e58348525fe1
Jason <18665308713@163.com>
2020-06-09 15:13:05 +0800

    添加detail页面

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import  { actionCreators }  from  './store'; 
+import {
+    DetailWrapper,
+    Header,
+    Content
+} from './style'
 
 class Detail extends Component {
     render() { 
+        const { title, content } = this.props
         return ( 
-            <div> detail</div>
+            <DetailWrapper>
+                <Header>{title}</Header>
+                <Content dangerouslySetInnerHTML={{__html: content }} />
+            </DetailWrapper>
         );
     }
 }
- 
-export default Detail;
+
+const mapState = (state) => ({
+    title: state.getIn(['detail','title']),
+    content: state.getIn(['detail', 'content'])
+
+})
+
+export default connect(mapState,null)(Detail);
